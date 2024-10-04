888b02d9abe577f1c41ed4790083910d029559c3
justyfay <sakura.ustinya@yandex.ru>
2024-10-04 14:17:36 +0300

    fix: edit config

--- ms_collector/config.py
+++ ms_collector/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="allow")
 
     net_video_service_url: str
     country_link_service_url: str
@@ -13,15 +13,26 @@ class Settings(BaseSettings):
     db_user: str
     db_pass: str
 
-    rmq_user: str
-    rmq_password: str
-    rmq_vhost: str
-
     log_level: str
 
     @property
     def database_url(self) -> str:
         return f"postgresql+asyncpg://{self.db_host}:{self.db_port}/{self.db_name}"
+
+
+class CelerySettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="allow")
+
+    rmq_user: str
+    rmq_password: str
+    rmq_vhost: str
+
+    default_exchange_name: str
+    default_queue_name: str
+    default_routing_key: str
+
+    product_events_queue_name: str
+    product_events_routing_key: str
 
     @property
     def broker_url(self) -> str:
@@ -29,3 +40,4 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+celery_settings = CelerySettings()
--- ms_collector/src/tasks/celery_app.py
+++ ms_collector/src/tasks/celery_app.py
@@ -3,44 +3,37 @@ from datetime import timedelta
 from celery import Celery
 from kombu import Queue, Exchange
 
-from config import settings
+from config import celery_settings
 
 celery: Celery = Celery(
     name="tasks",
-    broker=settings.broker_url,
+    broker=celery_settings.broker_url,
     include=["src.tasks.tasks"],
 )
 celery.conf.enable_utc = True
 celery.conf.timezone = "UTC"
 
-default_exchange_name = 'default'
-default_queue_name = 'default'
-default_routing_key = 'default'
-
-product_events_queue_name = 'product_events'
-product_events_routing_key = 'product_events'
-
-default_exchange = Exchange(default_exchange_name, type='direct')
+default_exchange = Exchange(celery_settings.default_exchange_name, type='direct')
 
 default_queue = Queue(
-    default_queue_name,
-    default_exchange,
-    routing_key=default_routing_key
+    name=celery_settings.default_queue_name,
+    exchange=default_exchange,
+    routing_key=celery_settings.default_routing_key
 )
 
 product_events = Queue(
-    product_events_queue_name,
-    default_exchange,
-    routing_key=product_events_routing_key
+    name=celery_settings.product_events_queue_name,
+    exchange=default_exchange,
+    routing_key=celery_settings.product_events_routing_key
 )
 
 celery.conf.task_queues = (default_queue, product_events)
-celery.conf.task_default_exchange = default_exchange_name
-celery.conf.task_default_queue = default_queue_name
+celery.conf.task_default_exchange = celery_settings.default_exchange_name
+celery.conf.task_default_queue = celery_settings.default_queue_name
 
 celery.conf.task_routes = {
     'task_run': {
-        'queue': default_queue_name
+        'queue': celery_settings.default_queue_name
     },
 }
 
